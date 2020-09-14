a95e9977ff038f644514128b8f50734e53a8657b
Vijay Tiwari <vijay.tiwari.vj7>
2020-09-14 22:02:45 +0530

    Updates

---
+++ precache-manifest.19a3cdb5531a5f6ae56580ddca5cd163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d29649f7cfd116573d592d39ecb32b8",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "04a9b0bf9b9ffdd60cb5",
+    "url": "/drum-machine/static/css/main.f8be7d24.chunk.css"
+  },
+  {
+    "revision": "8e29a2061841c825e6c3",
+    "url": "/drum-machine/static/js/2.1c6a7679.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.1c6a7679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a9b0bf9b9ffdd60cb5",
+    "url": "/drum-machine/static/js/main.714faffc.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
